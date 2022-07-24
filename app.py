43307ed91e5dca3fa0977d8f93685de13b5cdff4
Anastasia Slabucho <anastasia.slabucho@gmail.com>
2022-07-24 09:18:31 +0200

    adding routing

--- app.py
+++ app.py
@@ -69,5 +69,15 @@ def display():  # код приложения сюда
     return render_template('index.html', graphJSON=graphJSON)
 
 
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./orbe')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
+
+
 if __name__ == '__main__':
     app.run(debug=True)
