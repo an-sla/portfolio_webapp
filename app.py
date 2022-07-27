aa4b4322213e89080eed809da87811eb18b7971e
Anastasia Slabucho <anastasia.slabucho@gmail.com>
2022-07-27 12:25:45 +0200

    cache removal

--- app.py
+++ app.py
@@ -101,5 +101,14 @@ def muottas_muragl():
     return render_template('Celerina(muottas_muragl).html')
 
 
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
 if __name__ == '__main__':
     app.run(debug=True)
